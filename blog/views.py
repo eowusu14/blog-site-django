9e0062ff7c0964fb0df6c2c4bc2d0b8ecd1b1ceb
eowusu14 <owusuboateng149@gmail.com>
2023-12-21 21:48:36 +0000

    fix post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.http import Http404
 
 # Create your views here.
 def post_list(request):
@@ -7,3 +8,13 @@ def post_list(request):
     return render(request,
                   'blog/post/list.html',
                   {'posts':posts})
+    
+    
+def post_detail(request, id):
+    post = get_object_or_404(Post,
+                                 id=id,
+                                 status=Post.Status.PUBLISHED)
+       
+    return render(request,
+                      'blog/post/detail.html',
+                      {'post': post})
